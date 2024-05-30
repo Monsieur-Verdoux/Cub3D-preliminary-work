35111d403c182cdc7f91dfc5ebf1963339c53b71
Andrey Kovalevskiy <akovalev@student.hive.fi>
2024-05-30 20:51:27 +0300

    continuing map validation

--- parsing.c
+++ parsing.c
@@ -6,7 +6,7 @@
 /*   By: akovalev <akovalev@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/29 17:08:41 by akovalev          #+#    #+#             */
-/*   Updated: 2024/05/29 20:55:27 by akovalev         ###   ########.fr       */
+/*   Updated: 2024/05/30 20:50:56 by akovalev         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -53,6 +53,81 @@ int	check_arguments(int argc, char **argv, t_map *map)
 	}
 	return (0);
 }
+
+int	check_lines(t_map *map)
+{
+	size_t	y;
+	size_t	x;
+	char	*str;
+	char	*pr_str;
+	char	*nxt_str;
+	int		start_pos;
+
+	y = 0;
+	start_pos = 0;
+	printf("\nabout to check lines\n");
+	while (y < map->map_copy.len)
+	{
+		str = *(char **)vec_get(&map->map_copy, y);
+		//printf("%s", *(char **)(vec_get(&map->map_copy, n)));
+		x = 0;
+		if (str[x] == ' ')
+			x++;
+		//printf("we are now at %s", *(char **)(vec_get(&map->map_copy, y)));
+		while (str[x])
+		{
+			//printf("%c", *str);
+			if (str[x] != 'N' && str[x] != 'S' && str[x] != 'E' && str[x] != 'W' && str[x] != '1' && str[x] != ' ' && str[x] != '0' && str[x] != '\n')
+				printf("Disallowed characters in the map\n");
+			if (str[x] == ' ')
+			{
+				if ((str[x - 1] != '1' && str[x - 1] != ' ') || (str[x + 1] != '1' && str[x + 1] != ' ' && str[x + 1] != '\0' && str[x + 1] != '\n'))
+					printf("Wrong character at %s\n", &str[x]);
+			}
+			else if (str[x] == 'N' || str[x] == 'S' || str[x] == 'E' || str[x] == 'W')
+			{
+				start_pos++;
+				if (start_pos > 1)
+				{
+					printf("Only one starting position marker allowed\n");
+					return (1);
+				}
+			}
+			if (str[x] == '0' || str[x] == 'N' || str[x] == 'S' || str[x] == 'E' || str[x] == 'W')
+			{
+				if (y)
+					pr_str = *(char **)vec_get(&map->map_copy, y - 1);
+				if (y < map->map_copy.len - 1)
+					nxt_str = *(char **)vec_get(&map->map_copy, y + 1);
+				if (str[x + 1] == ' ' || str[x + 1] == '\n')
+				{
+					printf("Wrong character after %s\n", &str[x]);
+					return (1);
+				}
+				if ((y && (pr_str[x] == ' ' || pr_str[x] == '\n')) || y == 0)
+				{
+					printf("Wrong character above %s\n", &str[x]);
+					return (1);
+				}
+				if ((x && str[x - 1] == ' ') || x == 0)
+				{
+					printf("Wrong character before %s\n", &str[x]);
+					return (1);
+				}
+				if ((y < map->map_copy.len - 1 && (nxt_str [x] == ' ' || nxt_str [x] == '\n'|| nxt_str [x] == '\0')) || y == map->map_copy.len - 1)
+				{
+					printf("Wrong character below %s\n", &str[x]);
+					return (1);
+				}
+			}
+			x++;
+		}
+		//printf("\n");
+		y++;
+	}
+	return (0);
+}
+
 int	validate_map(t_map *map)
 {
 	char	*line;
@@ -199,7 +274,10 @@ int	validate_map(t_map *map)
 		line = get_next_line(map->fd);
 		map->line_count++;
 	}
-	//printf("\nLine 0 is\n%s\n", *(char **)(vec_get(&map->map_copy, 0)));
+	//printf("\nLine 0 is\n%s\n", *(char **)(vec_get(&map->map_copy, 0)))
+	if (check_lines(map))
+		return (1);
+	printf("\nMap size is %d lines\n", map->line_count);
 	printf("\nDone reading file\n");
 	return (0);
 }
